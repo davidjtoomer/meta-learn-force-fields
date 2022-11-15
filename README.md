c9f4beef71be1dc5d23e7069e208cb19150fd84f
David Toomer <djtoomer22@gmail.com>
2022-11-14 17:38:35 -0800

    edit: new link for faster download/preprocessing

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # Meta-Learning Force Fields
 
-Learning atomistic force fields and neural network potentials (NNP) using meta-learning. Final project for Stanford's [CS330: Deep Multi-Task and Meta-Learning](https://cs330.stanford.edu/).
+Learning atomistic neural network potentials (NNP) using meta-learning. Final project for Stanford's [CS330: Deep Multi-Task and Meta-Learning](https://cs330.stanford.edu/).
 
 ## Data
 
 This project uses the [ANI-1](https://chemrxiv.org/engage/chemrxiv/article-details/60c74aabbdbb896e2ba3940c) datasets. You can download the data by running:
 
 ```
-python download.py --dataset 1x 1ccx
+python download.py
 ```
--- download.py
+++ download.py
@@ -1,5 +1,4 @@
 import argparse
-import gzip
 import logging
 import os
 import shutil
@@ -18,8 +17,6 @@ parser.add_argument('--clear', action='store_true',
                     help='When present, clear the data directory.')
 parser.add_argument('--data_dir', type=str, default='data',
                     help='The directory in which to download the data.')
-parser.add_argument('--dataset', type=str, default=['1x'], nargs='+',
-                    choices=['1x', '1ccx'], help='The dataset to download.')
 args = parser.parse_args()
 
 if os.path.exists(args.data_dir):
@@ -30,32 +27,21 @@ if os.path.exists(args.data_dir):
 else:
     os.makedirs(args.data_dir)
 
-URLS = {
-    '1x': 'https://zenodo.org/record/4081694/files/292.hdf5.gz?download=1',
-    '1ccx': 'https://zenodo.org/record/4081692/files/293.hdf5.gz?download=1'
-}
+URL = 'https://springernature.figshare.com/ndownloader/files/18112775'
 
-for dataset in args.dataset:
-    file_path = os.path.join(args.data_dir, f'ani-{dataset}.hdf5.gz')
-    logger.info(f'Downloading {URLS[dataset]} to {file_path}...')
-    r = requests.get(URLS[dataset], stream=True)
-    if r.status_code == 200:
-        file_size = int(r.headers.get('Content-Length', 0))
-        progress_bar = tqdm.tqdm(total=file_size, unit='B', unit_scale=True)
-        with open(file_path, 'wb') as f:
-            for chunk in r.iter_content(chunk_size=1024):
-                if chunk:
-                    f.write(chunk)
-                    progress_bar.update(1024)
-        progress_bar.close()
-        logger.info(f'Downloaded {URLS[dataset]} to {file_path}.')
-        logger.info(f'Unzipping {file_path}...')
-        with gzip.open(file_path, 'rb') as f_in:
-            with open(file_path[:-3], 'wb') as f_out:
-                shutil.copyfileobj(f_in, f_out)
-        logger.info(f'Successfully unzipped {file_path}.')
-        logger.info(f'Removing {file_path}...')
-        os.remove(file_path)
-    else:
-        logger.error(
-            f'Error downloading dataset {dataset} to {URLS[dataset]}.')
+file_path = os.path.join(args.data_dir, f'ani.h5')
+logger.info(f'Downloading {URL} to {file_path}...')
+r = requests.get(URL, stream=True)
+if r.status_code == 200:
+    file_size = int(r.headers.get('Content-Length', 0))
+    progress_bar = tqdm.tqdm(total=file_size, unit='B', unit_scale=True)
+    with open(file_path, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=1024):
+            if chunk:
+                f.write(chunk)
+                progress_bar.update(1024)
+    progress_bar.close()
+    logger.info(f'Successfully downloaded {URL} to {file_path}.')
+else:
+    logger.error(
+        f'Error downloading {URL} to {file_path}.')
