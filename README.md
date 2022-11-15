028dd38e8be94123ffacee3dd36c5480b8229b72
David Toomer <djtoomer22@gmail.com>
2022-11-14 21:10:46 -0800

    edit: change preprocessing from directory structure to new h5 file for storage efficiency

--- README.md
+++ README.md
@@ -2,6 +2,28 @@
 
 Learning atomistic neural network potentials (NNP) using meta-learning. Final project for Stanford's [CS330: Deep Multi-Task and Meta-Learning](https://cs330.stanford.edu/).
 
+## Environment
+
+You can use [conda](https://docs.conda.io/en/latest/) or [pip](https://pypi.org/project/pip/) to run the code in this repository. For conda, run the following commands:
+
+```
+conda env create -f environment.yml
+conda activate meta-learn-force-fields
+```
+
+For pip, first create and activate a virtual environment:
+
+```
+python -m venv venv/
+source venv/bin/activate
+```
+
+and then install all of the requirements:
+
+```
+pip install -r requirements.txt
+```
+
 ## Data
 
 This project uses the [ANI-1](https://chemrxiv.org/engage/chemrxiv/article-details/60c74aabbdbb896e2ba3940c) datasets. You can download the data by running:
@@ -12,36 +34,27 @@ python download.py
 
 ## Preprocessing
 
-We provide a script for preprocessing the ANI-I dataset file into ANI-1x and ANI-1ccx, where each dataset is cleaned of NaN values. To preprocess the data, run:
+We provide a script for preprocessing the ANI-1 dataset file into ANI-1x and ANI-1ccx, where each dataset is cleaned of NaN values and extraneous data. To preprocess the data, run:
 
 ```
 python preprocess.py
 ```
 
-Furthermore, we sort the files into a directory structure well suited for multi-task and meta-learning. Within the meta-learning framework, we consider each molecule to be a task. Individual configurations of a given molecule, along with their respective potential energies, are samples of the task. After preprocessing, we structure our data directory as
-
-```
-meta-learn-force-fields
-└───data
-    └───DATASET_NAME
-        └───MOLECULE_NAME
-            │   0.pt
-            │   1.pt
-            │   ...
-```
-
-Each configuration file contains the attributes <code>atomic_numbers</code>, <code>coordinates</code>, and <code>energy</code> stored as [PyTorch tensors](https://pytorch.org/docs/stable/tensors.html). You can load any one of these configurations like so:
+This will generate two [HDF5 files](https://docs.h5py.org/en/stable/index.html), one for ANI-1x and another for ANI-1ccx. Each HDF5 file is structured with molecule names as [groups](https://docs.h5py.org/en/stable/high/group.html) at the top level. Each group contains [datasets](https://docs.h5py.org/en/stable/high/dataset.html) with the attributes <code>atomic_numbers</code>, <code>coordinates</code>, and <code>energy</code>. You can access the data for a specific molecule in your code as follows:
 
 ```python
-import torch
+import h5py
+import numpy as np
 
 ...
 
-task = 'C1H1N1'
-task_sample = torch.load(f'data/1x/{task}/0.pt')
-atomic_numbers = task_sample['atomic_numbers']
-coordinates = task_sample['coordinates']
-energy = task_sample['energy']
+dataset = '1x'
+molecule_name = 'C1H1N1'
+with h5py.File(f'data/ani{dataset}.h5') as f:
+    molecule = f[molecule_name]
+    atomic_numbers = np.array(molecule['atomic_numbers'])
+    coordinates = np.array(molecule['coordinates'])
+    energy = np.array(molecule['energy'])
 
 ...
 ```
--- preprocess.py
+++ preprocess.py
@@ -4,7 +4,6 @@ import os
 
 import h5py
 import numpy as np
-import torch
 import tqdm
 
 logging.basicConfig(
@@ -22,38 +21,32 @@ if not os.path.exists(args.data_dir):
         f'Data directory "{args.data_dir}" does not exist. Consider running "python download.py --data_dir {args.data_dir}" first.')
     exit(1)
 
-os.makedirs(os.path.join(args.data_dir, '1x'))
-os.makedirs(os.path.join(args.data_dir, '1ccx'))
-
 file_path = os.path.join(args.data_dir, 'ani.h5')
+file_path_1x = os.path.join(args.data_dir, 'ani1x.h5')
+file_path_1ccx = os.path.join(args.data_dir, 'ani1ccx.h5')
+
 logger.info(f'Preprocessing {file_path}...')
 with h5py.File(file_path, 'r') as f:
-    for name, molecule in tqdm.tqdm(f.items(), leave=False):
-        directory_1x = os.path.join(args.data_dir, '1x', name)
-        directory_1ccx = os.path.join(args.data_dir, '1ccx', name)
+    with h5py.File(file_path_1x, 'w') as f_1x:
+        with h5py.File(file_path_1ccx, 'w') as f_1ccx:
+            for name, molecule in tqdm.tqdm(f.items(), leave=False):
+                atomic_numbers = molecule['atomic_numbers']
+                coordinates = molecule['coordinates']
+                dft_energies = molecule['wb97x_dz.energy']
+                ccsd_energies = molecule['ccsd(t)_cbs.energy']
 
-        atomic_numbers = torch.Tensor(np.array(molecule['atomic_numbers']))
-        coordinates = torch.Tensor(np.array(molecule['coordinates']))
-        dft_energies = torch.Tensor(np.array(molecule['wb97x_dz.energy']))
-        ccsd_energies = torch.Tensor(np.array(molecule['ccsd(t)_cbs.energy']))
+                group_1x = f_1x.create_group(name)
+                group_1x.create_dataset('atomic_numbers', data=atomic_numbers)
+                group_1x.create_dataset('coordinates', data=coordinates)
+                group_1x.create_dataset('energy', data=dft_energies)
 
-        os.makedirs(directory_1x)
-        if torch.isnan(ccsd_energies).sum() < ccsd_energies.shape[0]:
-            os.makedirs(directory_1ccx)
-
-        ccsd_i = 0
-        for i in range(coordinates.shape[0]):
-            torch.save({
-                'atomic_numbers': atomic_numbers,
-                'coordinates': coordinates[i],
-                'energy': dft_energies[i],
-            }, os.path.join(directory_1x, f'{i}.pt'))
-
-            if not torch.isnan(ccsd_energies[i]):
-                torch.save({
-                    'atomic_numbers': atomic_numbers,
-                    'coordinates': coordinates[i],
-                    'energy': dft_energies[i],
-                }, os.path.join(directory_1ccx, f'{ccsd_i}.pt'))
-                ccsd_i += 1
+                ccsd_nan = np.isnan(ccsd_energies)
+                if ccsd_nan.sum() < ccsd_energies.shape[0]:
+                    group_1ccx = f_1ccx.create_group(name)
+                    group_1ccx.create_dataset(
+                        'atomic_numbers', data=atomic_numbers)
+                    group_1ccx.create_dataset(
+                        'coordinates', data=coordinates[~ccsd_nan])
+                    group_1ccx.create_dataset(
+                        'energy', data=ccsd_energies[~ccsd_nan])
 logger.info(f'Successfully preprocessed {file_path}.')
