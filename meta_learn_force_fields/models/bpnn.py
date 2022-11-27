52d849b25d3ab9bf1528e927b80c51db688336bb
David Toomer <djtoomer22@gmail.com>
2022-11-26 22:13:38 -0800

    edit: update featurizer and bpnn to match new features

--- meta_learn_force_fields/models/bpnn.py
+++ meta_learn_force_fields/models/bpnn.py
@@ -1,12 +1,15 @@
+from typing import List
+
 import torch
 
 from .featurizer import Featurizer, FeaturizerConfig
 
 
 class BPNN(torch.nn.Module):
-    def __init__(self, num_layers: int, feature_config: FeaturizerConfig):
+    def __init__(self, num_layers: int, atomic_numbers: List[int], feature_config: FeaturizerConfig):
         super().__init__()
         self.num_layers = num_layers
+        self.atomic_numbers = atomic_numbers
         self.feature_config = feature_config
 
         self.featurizer = Featurizer(self.feature_config)
@@ -16,10 +19,16 @@ class BPNN(torch.nn.Module):
             atomic_numbers: torch.Tensor,
             coordinates: torch.Tensor,
             parameters: dict) -> torch.Tensor:
-        x = self.featurizer(atomic_numbers, coordinates)
-        for i in range(self.num_layers):
-            x = torch.nn.functional.linear(
-                x, parameters[f'weight_{i}'], parameters[f'bias_{i}'])
-            if i < self.num_layers - 1:
-                x = torch.nn.functional.silu(x)
-        return x.sum(dim=-2).squeeze(-1)
+        x = self.featurizer(coordinates)
+        energies = torch.empty(coordinates.shape[0], coordinates.shape[1])
+        for atomic_number in self.atomic_numbers:
+            indices = torch.where(atomic_numbers == atomic_number)[0]
+            atoms = x[:, indices]
+            for i in range(self.num_layers):
+                atoms = torch.nn.functional.linear(
+                    atoms, parameters[f'atom_{atomic_number}_weight_{i}'], parameters[f'atom_{atomic_number}_bias_{i}'])
+                if i < self.num_layers - 1:
+                    atoms = torch.nn.functional.silu(atoms)
+            atoms = atoms.squeeze(-1)
+            energies[:, indices] = atoms
+        return energies.sum(dim=-1)
